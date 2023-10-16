06ad1c52e0e1b537ca84c9c72cf2722408ee2c26
ShubhamBhut <shubhamprecious1@gmail.com>
2023-10-16 07:26:13 +0530

    Set basic structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,39 @@
+pub trait Bond {
+    fn valuation(&self) ->f32;
+    fn holding_period_return(&self) ->f32;
+    fn yeild_to_maturity(&self) ->f32;
+}
+
+pub struct CorporateBond {
+    coupon_rate: f32,
+    dicount_rate: f32,
+    maturity: u32,
+    face_value: f64,
+    CompoundingFreq: CompoundingFreq,
+}
+
+enum CompoundingFreq {
+    Annual,
+    Semiannual,
+    Quarterly,
+    Monthly
+}
+
+todo!("Implement all 3 methods for Corporate and ZeroCoupon Bonds");
+impl Bond for CorporateBond {
+    fn valuation(&self) ->f32 {
+        0.0
+    }
+
+    fn holding_period_return(&self) ->f32 {
+        0.0
+    }
+
+    fn yeild_to_maturity(&self) ->f32 {
+        0.0
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
